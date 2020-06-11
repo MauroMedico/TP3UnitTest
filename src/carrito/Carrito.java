0fa0544c96f91bf5e6a7759640a6cedba2194594
Matias Videla <mvidela@globalis-sa.com>
2020-06-11 16:29:44 -0300

    TestU2

--- src/carrito/Carrito.java
+++ src/carrito/Carrito.java
@@ -11,6 +11,16 @@ public class Carrito {
   public static boolean ComprobarCantidadProductosMayorCero(int cantProductos) {
       
   return cantProductos>0;
+  }
+  //Creo un producto con stock
+  static Producto a = new Producto(001, "Celular", 10000,3);
+  public static int AgregarProductosNoMayorCantidadStock(int idProducto,int cantAAgregar) {
+        if (idProducto == a.idProducto && cantAAgregar <= a.stock ) {
+          return a.stock = a.stock - cantAAgregar;
+                  
+      }
+        return -1;
+      
     }
     
 }
---
+++ src/carrito/Producto.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrito;
+
+/**
+ *
+ * @author mativ
+ */
+public class Producto {
+   static int idProducto;
+   static String nombre;
+   static float precio;
+   static int stock;
+
+    public Producto(int idProducto, String nombre, float precio,int stock) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+}
